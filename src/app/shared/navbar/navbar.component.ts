0c24dae2a4a3c9e0892974301c3bdfd2e409560d
HarshPanchal26 <hrppanchal27@gmail.com>
2024-12-31 10:42:01 +0530

    modiied navbar fields

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FwdDialogComponent } from "../../component/fwd-dialog/fwd-dialog.component";
 
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FwdDialogComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
@@ -14,7 +15,14 @@ export class NavbarComponent {
   navigation = [
     { name: 'How do we help ?', href: '/how-do-we-help' },
     { name: 'Why Agency ?', href: '/why-agency' },
-    { name: 'Why ME ?', href: '/why-me' }
+    // { name: 'Why ME ?', href: '/why-me' }
     // { name: 'SEO', href: '/seo' },
    ];
+
+   isModalVisible : boolean = false
+
+   makeModalHide(){
+    this.isModalVisible = false
+   }
+   
 }
