d9593d542defd7cf75002d67dbf1ccfbb85a369d
HarshPanchal26 <hrppanchal27@gmail.com>
2025-01-01 10:58:06 +0530

    added build folder and we are live now !

---
+++ docs/browser/chunk-SJUHFG5T.js
@@ -0,0 +1,1 @@
+import{Aa as n,Na as o,Oa as m,Wa as p}from"./chunk-LLCI4WSQ.js";var a=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-why-me"]],decls:2,vars:0,template:function(e,i){e&1&&(o(0,"p"),p(1,"why-me works!"),m())},encapsulation:2})};export{a as WhyMEComponent};
---
+++ docs/server/chunk-OH2FAFP3.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{Pa as o,Qa as m,Ya as p,za as n}from"./chunk-DS3NH7LA.mjs";import"./chunk-X2SEQXRR.mjs";var a=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-why-me"]],decls:2,vars:0,template:function(e,i){e&1&&(o(0,"p"),p(1,"why-me works!"),m())},encapsulation:2})};export{a as WhyMEComponent};
